9bba98342ede545d0965cfd12798bb1a12e91cc5
Mateus Auler <mateusauler2@gmail.com>
2026-03-13 11:28:17 -0300

    Bump app version to 0.6.0 and clean test import
    Update Android and Dart package versions for a new release: android/app/build.gradle.kts versionCode 4 -> 6 and versionName 0.4.0 -> 0.6.0, and pubspec.yaml version 0.4.0 -> 0.6.0. Also remove an unused import (package:flutter/services.dart) from integration_test/app_test.dart to clean up the test file.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.6.0"
     }
 
     signingConfigs {
